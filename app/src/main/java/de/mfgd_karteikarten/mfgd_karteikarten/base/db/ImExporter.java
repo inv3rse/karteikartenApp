697aaf9771132424a5f3c1a8cad639d6964f48de
inv3rse <3sr3vn1@gmail.com>
2016-03-02 16:41:27 +0100

    ImExporter Angelegt

---
+++ app/src/main/java/de/mfgd_karteikarten/mfgd_karteikarten/base/db/ImExporter.java
@@ -0,0 +1,12 @@
+package de.mfgd_karteikarten.mfgd_karteikarten.base.db;
+
+public class ImExporter {
+
+    private TopicEditor topicEditor;
+
+    public ImExporter(TopicEditor topicEditor)
+    {
+
+    }
+
+}
