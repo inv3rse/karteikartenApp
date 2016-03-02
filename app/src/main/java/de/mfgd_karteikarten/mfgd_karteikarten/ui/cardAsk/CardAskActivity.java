df56307b2fb12a33aa7ed821f43b399becb7383d
Guangyu Ge <geguangyu.vanessa@gmail.com>
2016-03-02 11:03:58 +0100

    activtiy_card_ask Funktion hinzugefügt.

--- app/src/main/java/de/mfgd_karteikarten/mfgd_karteikarten/ui/cardAsk/CardAskActivity.java
+++ app/src/main/java/de/mfgd_karteikarten/mfgd_karteikarten/ui/cardAsk/CardAskActivity.java
@@ -1,11 +1,14 @@
 package de.mfgd_karteikarten.mfgd_karteikarten.ui.cardAsk;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -20,6 +23,11 @@ public class CardAskActivity extends NucleusAppCompatActivity<CardAskPresenter> 
     public static final String CARDASK_EXTRA_CARDS = "CARDASK_EXTRA_CARDS";
     private EditText fragenstellen;
     private EditText antwortEditText;
+    private Button zeigeAntwortButton;
+    private Button bewertenButton1;
+    private Button bewertenButton2;
+    private ImageView imageView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,46 +36,66 @@ public class CardAskActivity extends NucleusAppCompatActivity<CardAskPresenter> 
 
         Bundle extras = getIntent().getExtras();
 
-        ArrayList<Integer> ids =  extras.getIntegerArrayList(CARDASK_EXTRA_CARDS);
+        ArrayList<Integer> ids = extras.getIntegerArrayList(CARDASK_EXTRA_CARDS);
         boolean cards = true;
-        if (ids == null)
-        {
+        if (ids == null) {
             cards = false;
-            ids =  extras.getIntegerArrayList(CARDASK_EXTRA_DECKS);
+            ids = extras.getIntegerArrayList(CARDASK_EXTRA_DECKS);
         }
 
-        if (ids != null)
-        {
+        if (ids != null) {
             setPresenterFactory(new CardAskPresenter.Factory(App.get(this), ids, cards));
-        }
-        else {
+        } else {
             Log.e("CardaskActivity", "gestartet ohne Cardask als Extra");
             finish();
         }
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         fragenstellen = (EditText) findViewById(R.id.FrageStellen);
         antwortEditText = (EditText) findViewById(R.id.AntwortEdit);
-        Button zeigeAntowrt = (Button) findViewById(R.id.zeigAntowrtButton);
-        Button bewertenButton1 = (Button) findViewById(R.id.bewertenButton1);
-        Button bewertenButton2 = (Button) findViewById(R.id.bewertenButton2);
+        imageView = (ImageView) findViewById(R.id.smiley);
+        zeigeAntwortButton = (Button) findViewById(R.id.zeigAnwortButton);
+        bewertenButton1 = (Button) findViewById(R.id.bewertenButton1);
+        bewertenButton2 = (Button) findViewById(R.id.bewertenButton2);
+        antwortEditText.setVisibility(View.INVISIBLE);
+        imageView.setVisibility(View.GONE);
+        bewertenButton1.setVisibility(View.GONE);
+        bewertenButton2.setVisibility(View.GONE);
 
-
-        antwortEditText.setVisibility(View.VISIBLE);
 
         bewertenButton1.setOnClickListener(v -> getPresenter().fertigpositv());
         bewertenButton2.setOnClickListener(v -> getPresenter().fertigpositv());
 
+        zeigeAntwortButton.setOnClickListener(v -> getPresenter().zeigeAntwort());
     }
 
     public void setQuestion(Card card) {
         fragenstellen.setText(card.getQuestion());
     }
+
     public void setAnswer(Card card) {
         fragenstellen.setText(card.getAnswer());
     }
 
-
+    public void setAnswerVisible(boolean answerVisible)
+    {
+        if (answerVisible)
+        {
+            bewertenButton1.setVisibility(View.VISIBLE);
+            bewertenButton2.setVisibility(View.VISIBLE);
+            zeigeAntwortButton.setVisibility(View.GONE);
+            antwortEditText.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            bewertenButton1.setVisibility(View.GONE);
+            bewertenButton2.setVisibility(View.GONE);
+            zeigeAntwortButton.setVisibility(View.VISIBLE);
+            antwortEditText.setVisibility(View.INVISIBLE);
+            imageView.setVisibility(View.GONE);
+        }
+    }
 }
