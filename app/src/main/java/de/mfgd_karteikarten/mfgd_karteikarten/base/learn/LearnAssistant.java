838e3b3beccd91e7d4b5e566750eec6900940801
inv3rse <3sr3vn1@gmail.com>
2016-03-02 12:21:57 +0100

    fehler in LearnAssistant behoben

--- app/src/main/java/de/mfgd_karteikarten/mfgd_karteikarten/base/learn/LearnAssistant.java
+++ app/src/main/java/de/mfgd_karteikarten/mfgd_karteikarten/base/learn/LearnAssistant.java
@@ -46,11 +46,6 @@ public class LearnAssistant implements LearnInterface {
 
     @Override
     public Card shuffleCard() {
-        if (currentCard != null)
-        {
-            cards.add(currentCard);
-        }
-
         return getNextCard();
     }
 
