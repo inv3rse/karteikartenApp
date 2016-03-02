df2c9fd5dfcbfcd968bde74717897659b2c6227e
inv3rse <3sr3vn1@gmail.com>
2016-03-02 12:19:22 +0100

    LearnAssistant für endloses lernen

--- app/src/main/java/de/mfgd_karteikarten/mfgd_karteikarten/base/learn/LearnAssistant.java
+++ app/src/main/java/de/mfgd_karteikarten/mfgd_karteikarten/base/learn/LearnAssistant.java
@@ -1,6 +1,10 @@
 package de.mfgd_karteikarten.mfgd_karteikarten.base.learn;
 
+import android.util.Log;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import de.mfgd_karteikarten.mfgd_karteikarten.data.Card;
 import io.realm.Realm;
@@ -8,31 +12,68 @@ import io.realm.Realm;
 public class LearnAssistant implements LearnInterface {
     private Realm realm;
     private List<Card> cards;
-    private int nextID = 1;
+    private Card currentCard;
+    private Random random;
 
     public LearnAssistant(Realm realm, List<Card> cards) {
         this.realm = realm;
         this.cards = cards;
-
+        this.random = new Random();
     }
 
     @Override
     public boolean hasNextCard() {
-        return false;
+        return !cards.isEmpty();
     }
 
     @Override
     public Card getNextCard() {
-        return null;
+
+        // Alle karten mit einem negativen Rating werden mehrfach in die Liste eingetragen
+        ArrayList<Integer> cardIndexes = new ArrayList<>();
+        for (int i = 0; i < cards.size(); ++i)
+        {
+            int j = 0;
+            do {
+                cardIndexes.add(i);
+                j--;
+            } while (j > cards.get(i).getRating());
+        }
+
+        currentCard = cards.get(cardIndexes.get(random.nextInt(cardIndexes.size() - 1)));
+        return currentCard;
     }
 
     @Override
     public Card shuffleCard() {
-        return null;
+        if (currentCard != null)
+        {
+            cards.add(currentCard);
+        }
+
+        return getNextCard();
     }
 
     @Override
     public void gradeCurrentCard(boolean positive) {
-
+        if (currentCard != null)
+        {
+            if (positive)
+            {
+                currentCard.setRating(currentCard.getRating() + 1);
+            }
+            else
+            {
+                currentCard.setRating(currentCard.getRating() + 1);
+            }
+            if (currentCard.getId() != Card.UNKNOWN_ID)
+            {
+                realm.copyToRealmOrUpdate(currentCard);
+            }
+            else
+            {
+                Log.e("LearnAssistant", "Kann Karte ohne ID nicht updaten");
+            }
+        }
     }
 }
